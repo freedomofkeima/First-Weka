a9a641666670cca91889bbb9b5465cc9d2bfddb7
M Andri Eka F <13511088@std.stei.itb.ac.id>
2014-12-04 22:07:54 +0700

    Masukin list apa aja yg harus dibuat u/ algoritma k-means

--- src/clusterer/myPartitionalClusterer.java
+++ src/clusterer/myPartitionalClusterer.java
@@ -8,6 +8,16 @@ package clusterer;
  *
  */
 
-public class myPartitionalClusterer {
+public class myPartitionalClusterer extends RandomizableClusterer
+implements NumberOfClustersRequestable, WeightedInstancesHandler, TechnicalInformationHandler {
+
+//cara kerja k-means
+//TENTUIN BAKAL ADA BERAPA CLUSTER
+//Randomize Center nya yg mana aja -> attribut center nyolong dari data yg di randomize
+//sisa data diclustering ke center2 yg terdekat -> buat algoritma comparision nya
+//kalo udah semua di clustering, hitung ulang centernya -> pake fungsi ngitung center berdasarkan data yang ada -> sama kayak fungsi awal pas randomize
+//hitung jarak isi cluster ke center sendiri dan center orang lain, kalo center orang lain lebih deket -> pindahin ke sana
+//ulangi dari tahap 2
+//tresshold kalo gak ada yg berubah2 clusternya atau udh x iterasi(maunya)
 
 }
